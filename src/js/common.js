66a4a333ca1e659bda8e3845b59dadd89d452b6f
Alexander Ruzhickii <ruzhickiisasha@gmail.com>
2019-03-12 22:39:04 +0200

    fixed bugs

--- src/js/common.js
+++ src/js/common.js
@@ -1,8 +1,16 @@
-document.body.onload = function () {
-    setTimeout(function() {
-        var preloader = document.getElementById('preloader');
-        if( !preloader.classList.contains('done') ) {
-            preloader.classList.add('done');
-        }
-    }, 2000);
-};
+// document.body.onload = function () {
+//     setTimeout(function() {
+//         var preloader = document.getElementById('preloader');
+//         if( !preloader.classList.contains('done') ) {
+//             preloader.classList.add('done');
+//         }
+//     }, 1000);
+// };
+
+
+$(window).load(function () {
+    setTimeout(function () {
+        $('.preloader').fadeOut('slow');
+
+    },1000);
+});
